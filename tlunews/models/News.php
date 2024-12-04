f83da993c795f339b1948c60865f453c70cdaea8
dotridung2004 <dtd16102004@gmail.com>
2024-12-04 17:15:18 +0700

    BTTH2

--- tlunews/models/News.php
+++ tlunews/models/News.php
@@ -22,7 +22,36 @@
         public function getTitle(){
             return $this->title;
         }
+        public function getContent(){
+            return $this->content;
+        }
 
+        public function getImage(){
+            return $this->image;
+        }
+        public function getCreatedAt(){
+            return $this->created_at;
+        }
+        public function getCategoryId(){
+            return $this->category_id;
+        }
+        public function setId($id){
+            $this->id = $id;
+        }
+        public function setTitle($title){
+            $this->title = $title;
+        }
+        public function setContent($content){
+            $this->content = $content;
+        }
+        public function setImage($image){
+            $this->image = $image;
+        }
+        public function setCreated_at($created_at){
+            $this->created_at = $created_at;
+        }
+        public function setCategoryId($category_id){
+            $this->category_id = $category_id;
+        }
     }
-
 ?>
